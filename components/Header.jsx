17d0fa15a506ecca9048a81fcd5d8569fddbafaf
Phuc Nguyen Hoang <87425340+HPhucDev@users.noreply.github.com>
2021-12-30 08:05:02 +0700

    final

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { getCategories } from '../services';
 import logo from '../public/Logo.png';
+
 const Header = () => {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
